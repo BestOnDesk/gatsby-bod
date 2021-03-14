c5ef2c97ae02d7e5600b09552ea6372c4ff159bf
Igor Zanella <igorzanella.dev@gmail.com>
2021-03-14 23:21:33 +0100

    style: prettify eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,29 +2,29 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
   },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
   ],
   globals: {
     Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    SharedArrayBuffer: "readonly",
   },
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: "module",
   },
   plugins: ["prettier", "react", "@typescript-eslint"],
   rules: {
     "prettier/prettier": "error",
-    "react/display-name": 0
-  }
+    "react/display-name": 0,
+  },
 };
