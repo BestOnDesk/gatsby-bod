2287ec4a30d1fa2769b1cb82f11274064501c404
Igor Zanella <igorzanella.dev@gmail.com>
2021-03-29 12:15:55 +0200

    feat: add Footer

--- src/components/Layout/Footer/index.style.ts
+++ src/components/Layout/Footer/index.style.ts
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { HoverFlipItemWrapper } from "../../HoverFlip/index.style";
+import { media } from "styled-bootstrap-grid";
 
 const FooterArea = styled.div`
   padding: 40px 0 !important;
@@ -27,4 +29,94 @@ const FollowTitle = styled.h5`
 
 const CopyrightArea = styled.div``;
 
-export { FooterArea, FooterTop, Inner, FollowTitle, CopyrightArea };
+const CopyrightLeft = styled.div`
+  display: flex !important;
+  align-items: center !important;
+  flex-wrap: wrap !important;
+`;
+
+const Logo = styled.div`
+  margin-right: 53px !important;
+  @media ${(props) => props.theme.layouts.sm} {
+    flex-basis: 100% !important;
+    margin-bottom: 20px !important;
+  }
+`;
+
+const MainMenu = styled.ul`
+  ${(props) => props.theme.extends.listStyle}
+  display: flex !important;
+  flex-wrap: wrap !important;
+  justify-content: start !important;
+  li {
+    margin: 0 18px !important;
+
+    @media ${(props) => props.theme.layouts.sm} {
+      margin: 0 5px !important;
+    }
+
+    &:first-child {
+      margin-left: 0 !important;
+    }
+
+    a {
+      color: ${(props) => props.theme.colors.midgray} !important;
+      font-size: 16px !important;
+      line-height: 22px !important;
+      display: block !important;
+      font-weight: 500 !important;
+      height: auto !important;
+      line-height: initial !important;
+      ${(props) => props.theme.extends.transition}
+    }
+
+    ${HoverFlipItemWrapper} {
+      span {
+        &::before {
+          color: ${(props) => props.theme.colors.midgray} !important;
+        }
+        &::after {
+          color: ${(props) => props.theme.colors.primary} !important;
+        }
+      }
+    }
+  }
+`;
+
+const CopyrightRight = styled.div`
+  text-align: left !important;
+
+  p {
+    color: ${(props) => props.theme.colors.midgray} !important;
+  }
+
+  @media only screen and (max-width: 767px) {
+    margin-top: 20px !important;
+  }
+
+  @media only screen and (min-width: 768px) and (max-width: 991px) {
+    margin-top: 20px !important;
+  }
+
+  ${media.lg`
+    text-align: right !important;
+  `}
+`;
+
+const CopyrightText = styled.p`
+  font-size: ${(props) => props.theme.fonts.size.b3} !important;
+  line-height: ${(props) => props.theme.fonts.height.b3} !important;
+`;
+
+export {
+  FooterArea,
+  FooterTop,
+  Inner,
+  FollowTitle,
+  CopyrightArea,
+  CopyrightLeft,
+  Logo,
+  MainMenu,
+  CopyrightRight,
+  CopyrightText,
+};
--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -1,5 +1,15 @@
 import React from "react";
-import { CopyrightArea, FollowTitle, FooterArea, Inner } from "./index.style";
+import {
+  CopyrightArea,
+  CopyrightLeft,
+  CopyrightRight,
+  CopyrightText,
+  FollowTitle,
+  FooterArea,
+  Inner,
+  Logo,
+  MainMenu,
+} from "./index.style";
 import { Col, Container, Row } from "styled-bootstrap-grid";
 import SocialIcons from "components/SocialIcons";
 import {
@@ -8,6 +18,9 @@ import {
   LINKEDIN_URL,
   TELEGRAM_URL,
 } from "../../../constants/socials";
+import { Link } from "gatsby";
+import StaticImg from "../../StaticImg";
+import HoverFlip from "../../HoverFlip";
 
 const Footer = () => {
   return (
@@ -62,8 +75,35 @@ const Footer = () => {
       <CopyrightArea>
         <Container>
           <Row alignItems={"center"}>
-            <Col lg={9} md={12}></Col>
-            <Col lg={3} md={12}></Col>
+            <Col lg={9} md={12}>
+              <CopyrightLeft>
+                <Logo>
+                  <Link to={"/"}>
+                    <StaticImg
+                      imgStyle={{ maxHeight: "37px" }}
+                      alt="BestOnDesk"
+                      src={"logo/logo-light.png"}
+                      srcDark={"logo/logo-dark.png"}
+                    />
+                  </Link>
+                </Logo>
+                <MainMenu>
+                  <li>
+                    <HoverFlip to={"/chi-siamo"}>Chi siamo</HoverFlip>
+                  </li>
+                  <li>
+                    <HoverFlip to={"/"}>AGGIUNGERE ROBA</HoverFlip>
+                  </li>
+                </MainMenu>
+              </CopyrightLeft>
+            </Col>
+            <Col lg={3} md={12}>
+              <CopyrightRight>
+                <CopyrightText>
+                  Tutti i diritti riservati © {new Date().getFullYear()}
+                </CopyrightText>
+              </CopyrightRight>
+            </Col>
           </Row>
         </Container>
       </CopyrightArea>
