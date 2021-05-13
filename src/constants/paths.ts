f82eb02681aabe67459c0fb3db1602108f28888d
Igor Zanella <igorzanella.dev@gmail.com>
2021-05-13 02:59:04 +0200

    feat: add Iubenda cookie policy url in constants

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -1,4 +1,7 @@
 export const MAIN_URL = "https://bestondesk.com";
+export const COOKIE_POLICY_URL =
+  "https://www.iubenda.com/privacy-policy/58676891/cookie-policy";
+
 export const CATEGORY_PATH = "/category";
 export const POST_PATH = "";
 export const AUTHOR_PATH = "/author";
