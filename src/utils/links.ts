f6c87c087a7f23e521992b7c8a25606e89c243e4
Igor Zanella <igorzanella.dev@gmail.com>
2021-05-31 04:09:06 +0200

    feat: add getAuthorLinkWithPage

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -3,8 +3,16 @@ import { AUTHOR_PATH, POST_PATH } from "../constants/paths";
 const getCategoryLinkWithPage = (uri: string, page: number): string =>
   uri + page;
 
-const getPostLink = (slug: string): string => POST_PATH + "/" + slug;
+const getPostLink = (slug: string): string => `${POST_PATH}/${slug}`;
 
-const getAuthorLink = (slug: string): string => AUTHOR_PATH + "/" + slug;
+const getAuthorLink = (slug: string): string => `${AUTHOR_PATH}/${slug}`;
 
-export { getPostLink, getAuthorLink, getCategoryLinkWithPage };
+const getAuthorLinkWithPage = (slug: string, page: number): string =>
+  `${AUTHOR_PATH}/${slug}/${page}`;
+
+export {
+  getPostLink,
+  getAuthorLink,
+  getCategoryLinkWithPage,
+  getAuthorLinkWithPage,
+};
