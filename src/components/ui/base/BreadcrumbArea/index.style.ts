29645d28499b991f11e04f0225cc86c1a2794e07
Igor Zanella <igorzanella.dev@gmail.com>
2021-05-31 04:07:39 +0200

    feat: add author in BreadCrumbArea component

--- src/components/ui/base/BreadcrumbArea/index.style.ts
+++ src/components/ui/base/BreadcrumbArea/index.style.ts
@@ -1,9 +1,23 @@
 import styled from "styled-components";
 import { StyledTitle } from "../../../core/Title/index.style";
+import { AuthorInfo, StyledAboutAuthor } from "../AboutAuthor/index.style";
 
 const StyledBreadcrumbArea = styled.div`
   padding: 80px 0;
   background: ${(props) => props.theme.colors.lightest};
+
+  ${StyledAboutAuthor} {
+    border: none;
+    padding: 0;
+
+    ${AuthorInfo} {
+      ${StyledTitle} {
+        margin-bottom: 0;
+        font-size: 30px;
+        line-height: 34px;
+      }
+    }
+  }
 `;
 
 const Inner = styled.div`
--- src/components/ui/base/BreadcrumbArea/index.tsx
+++ src/components/ui/base/BreadcrumbArea/index.tsx
@@ -8,13 +8,16 @@ import {
 import { Col, Container, Row } from "styled-bootstrap-grid";
 import Title from "../../../core/Title";
 import { Link } from "gatsby";
+import { AuthorPreview } from "../../../../app-types/author";
+import AboutAuthor from "../AboutAuthor";
 
 export interface BreadcrumbAreaProps {
-  title: string;
-  breadcrumbs: {
+  title?: string;
+  breadcrumbs?: {
     name: string;
     link: string;
   }[];
+  author?: AuthorPreview;
 }
 
 const BreadcrumbArea = (props: BreadcrumbAreaProps) => {
@@ -22,31 +25,36 @@ const BreadcrumbArea = (props: BreadcrumbAreaProps) => {
     <StyledBreadcrumbArea>
       <Container>
         <Row>
-          <Col lg={12}>
-            <BreadcrumbsUl
-              itemScope
-              itemType={"https://schema.org/BreadcrumbList"}
-            >
-              {props.breadcrumbs.map((breadcrumb, i) => {
-                return (
-                  <BreadcrumbsLi
-                    key={i}
-                    itemProp={"itemListElement"}
-                    itemScope
-                    itemType={"https://schema.org/ListItem"}
-                  >
-                    {i > 0 && <i className="fas fa-chevron-right" />}
-                    <Link to={breadcrumb.link}>{breadcrumb.name}</Link>
-                  </BreadcrumbsLi>
-                );
-              })}
-            </BreadcrumbsUl>
-          </Col>
-          <Col lg={12}>
-            <Inner>
-              <Title level={1}>{props.title}</Title>
-            </Inner>
-          </Col>
+          {props.breadcrumbs && (
+            <Col lg={12}>
+              <BreadcrumbsUl
+                itemScope
+                itemType={"https://schema.org/BreadcrumbList"}
+              >
+                {props.breadcrumbs.map((breadcrumb, i) => {
+                  return (
+                    <BreadcrumbsLi
+                      key={i}
+                      itemProp={"itemListElement"}
+                      itemScope
+                      itemType={"https://schema.org/ListItem"}
+                    >
+                      {i > 0 && <i className="fas fa-chevron-right" />}
+                      <Link to={breadcrumb.link}>{breadcrumb.name}</Link>
+                    </BreadcrumbsLi>
+                  );
+                })}
+              </BreadcrumbsUl>
+            </Col>
+          )}
+          {props.title && (
+            <Col lg={12}>
+              <Inner>
+                <Title level={1}>{props.title}</Title>
+              </Inner>
+            </Col>
+          )}
+          {props.author && <AboutAuthor mainTitle author={props.author} />}
         </Row>
       </Container>
     </StyledBreadcrumbArea>
