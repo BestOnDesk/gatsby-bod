449c397d83f4daf5e2dc8b52ba4c392636b59070
Igor Zanella <igorzanella.dev@gmail.com>
2021-07-13 00:29:24 +0200

    feat: add redirects

--- src/create/create-pages.ts
+++ src/create/create-pages.ts
@@ -2,6 +2,7 @@ import { GatsbyNode } from "gatsby";
 import { createPosts } from "./create-posts";
 import { createCategories } from "./create-categories";
 import { createAuthors } from "./create-authors";
+import { createRedirects } from "./create-redirects";
 
 export const createPages: GatsbyNode["createPages"] = async ({
   actions,
@@ -10,4 +11,5 @@ export const createPages: GatsbyNode["createPages"] = async ({
   await createPosts(actions, graphql);
   await createCategories(actions, graphql);
   await createAuthors(actions, graphql);
+  await createRedirects(actions);
 };
---
+++ src/create/create-redirects.ts
@@ -0,0 +1,26 @@
+import { Actions } from "gatsby";
+
+import redirectsContent from "./redirects.json";
+
+const config = require("../../gatsby-config");
+const isEnvDevelopment = process.env.NODE_ENV === "development";
+
+export interface Redirect {
+  fromPath: string;
+  toPath: string;
+  isPermanent: boolean;
+}
+
+export const createRedirects = async (actions: Actions): Promise<void> => {
+  const redirects = (redirectsContent as unknown) as Redirect[];
+
+  redirects.forEach((redirect) => {
+    actions.createRedirect({
+      fromPath: redirect.fromPath,
+      toPath: redirect.toPath,
+      isPermanent: redirect.isPermanent,
+      statusCode: redirect.isPermanent ? 301 : 302,
+      redirectInBrowser: isEnvDevelopment,
+    });
+  });
+};
