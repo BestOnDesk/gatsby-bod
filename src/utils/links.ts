d9fbfc5eb52894e82d3ee1784085a0fff001f32c
Igor Zanella <igorzanella.dev@gmail.com>
2021-07-17 12:24:38 +0200

    feat: add trailing slashes to links

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -3,12 +3,12 @@ import { AUTHOR_PATH, POST_PATH } from "../constants/paths";
 const getCategoryLinkWithPage = (uri: string, page: number): string =>
   uri + page;
 
-const getPostLink = (slug: string): string => `${POST_PATH}/${slug}`;
+const getPostLink = (slug: string): string => `${POST_PATH}/${slug}/`;
 
-const getAuthorLink = (slug: string): string => `${AUTHOR_PATH}/${slug}`;
+const getAuthorLink = (slug: string): string => `${AUTHOR_PATH}/${slug}/`;
 
 const getAuthorLinkWithPage = (slug: string, page: number): string =>
-  `${AUTHOR_PATH}/${slug}/${page}`;
+  `${AUTHOR_PATH}/${slug}/${page}/`;
 
 export {
   getPostLink,
