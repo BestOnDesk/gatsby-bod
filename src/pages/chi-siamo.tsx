c273de1f9feebcadd361ef21878d68ee170af30d
Igor Zanella <igorzanella.dev@gmail.com>
2021-08-22 16:27:19 +0200

    feat: add text to chi-siamo page

--- src/pages/chi-siamo.tsx
+++ src/pages/chi-siamo.tsx
@@ -1,27 +1,87 @@
 import React from "react";
 import GlobalWrapper from "../components/core/GlobalWrapper";
 import SEO from "../components/core/SEO";
-import { Banner, Description, Inner } from "../styles/chi-siamo.style";
+import BreadcrumbArea from "components/ui/base/BreadcrumbArea";
+import PostListArea from "components/ui/base/PostListArea";
 import { Col, Container, Row } from "styled-bootstrap-grid";
-import Title from "../components/core/Title";
+import { AboutUs, Inner } from "styles/chi-siamo.style";
+import Title from "components/core/Title";
 
 export default () => {
   return (
     <GlobalWrapper withLayout>
       <SEO title={"Chi siamo"} description={"Chi siamo"} />
 
-      <Banner>
+      <BreadcrumbArea
+        title="Chi siamo"
+        breadcrumbs={[
+          { name: "BestOnDesk", link: "/" },
+          {
+            name: "Chi siamo",
+            link: "/chi-siamo",
+          },
+        ]}
+      />
+
+      <PostListArea>
         <Container>
           <Row>
-            <Col lg={12}>
-              <Inner>
-                <Title level={1}>Chi siamo</Title>
-                <Description>Ciao ciao ciao Ciao ciao ciao</Description>
-              </Inner>
+            <Col lg={8} xl={8}>
+              <AboutUs>
+                <Inner>
+                  <Title level={2}>
+                    Bestondesk é il tuo nuovo collega che sarà sempre dalla tua
+                    parte
+                  </Title>
+                  <Title level={3}>Perché?</Title>
+                  <p>
+                    Perché bbiamo accumulato oltre 20 anni di esperienza nel
+                    lavoro d’ufficio. Abbastanza per comprendere che le piccole
+                    cose fanno spesso la differenza tra una carriera di successo
+                    e uno stato di insoddisfazione.
+                  </p>
+                  <Title level={3}>Cosa troverai qui</Title>
+                  <p>
+                    Abbiamo deciso mettere la nostra esperienza a disposizione
+                    di tutti coloro che desiderano cambiare qualcosa,
+                    migliorarsi, o cominciare un nuovo percorso partendo dalle
+                    piccole cose come per esempio dalla creazione di una
+                    postazione di lavoro che sia l’espressione di te stesso.
+                  </p>
+                  <p>
+                    Del carattere, talento, competenza, serietà e
+                    professionalità che ti contraddistinguono prima di tutto
+                    come persona e poi come impiegato, manager, imprenditore o
+                    dipedente.
+                  </p>
+                  <Title level={3}>Cosa facciamo tutti i giorni</Title>
+                  <p>
+                    Testiamo decine e decine di prodotti per ufficio e
+                    continuiamo costantemente a fare nuove esperienze da
+                    condividere, ogni giorno alla ricerca dell’eccellenza,
+                    dell’unicità e dello stile da raggiungere attraverso la
+                    personalizzazione della postazione di lavoro accompagnata
+                    dalla crescita personale a 360°.
+                  </p>
+                  <Title level={3}>Our mission</Title>
+                  <p>
+                    La nostra missione è aiutarti ad avere successo attraverso
+                    te stesso!
+                  </p>
+                  <p>
+                    Mantenere viva la tua creatività costruendoci attorno
+                    l’ambiente perfetto in cui fare emergere le tue qualità.
+                  </p>
+                  <p>
+                    Perché la tua identità viene prima di qualsiasi altra cosa e
+                    tutto ciò che devi fare è non dimenticarlo mai.
+                  </p>
+                </Inner>
+              </AboutUs>
             </Col>
           </Row>
         </Container>
-      </Banner>
+      </PostListArea>
     </GlobalWrapper>
   );
 };
--- src/styles/chi-siamo.style.ts
+++ src/styles/chi-siamo.style.ts
@@ -1,38 +1,7 @@
 import styled from "styled-components";
-import { StyledTitle } from "../components/core/Title/index.style";
 
-const Banner = styled.div`
-  height: 670px;
-  display: flex;
-  align-items: flex-end;
-  padding-bottom: 80px;
-  position: relative;
+const Inner = styled.div``;
 
-  &::before {
-    position: absolute;
-    content: "";
-    background: linear-gradient(180deg, rgba(26, 26, 26, 0) 0%, #1a1a1a 100%);
-    opacity: 0.8;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    height: 80%;
-  }
-`;
+const AboutUs = styled.div``;
 
-const Inner = styled.div`
-  position: relative;
-  z-index: 2;
-
-  ${StyledTitle} {
-    color: #ffffff;
-    margin-bottom: 20px;
-  }
-`;
-
-const Description = styled.p`
-  margin-bottom: 0;
-  color: ${(props) => props.theme.colors.lighter};
-`;
-
-export { Banner, Inner, Description };
+export { Inner, AboutUs };
