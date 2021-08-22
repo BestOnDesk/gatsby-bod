1dd4f9fb4aa6084e4cda8c88075b5a2b5b87e4f5
Igor Zanella <igorzanella.dev@gmail.com>
2021-08-22 12:15:00 +0200

    feat: add chi-siamo page

---
+++ src/pages/chi-siamo/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import GlobalWrapper from "../../components/core/GlobalWrapper";
+import SEO from "../../components/core/SEO";
+import { Banner, Description, Inner } from "./index.style";
+import { Col, Container, Row } from "styled-bootstrap-grid";
+import Title from "../../components/core/Title";
+
+export default () => {
+  return (
+    <GlobalWrapper withLayout>
+      <SEO title={"Chi siamo"} description={"Chi siamo"} />
+
+      <Banner>
+        <Container>
+          <Row>
+            <Col lg={12}>
+              <Inner>
+                <Title level={1}>Chi siamo</Title>
+                <Description>Ciao ciao ciao Ciao ciao ciao</Description>
+              </Inner>
+            </Col>
+          </Row>
+        </Container>
+      </Banner>
+    </GlobalWrapper>
+  );
+};
