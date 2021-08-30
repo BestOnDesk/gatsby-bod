997356746fd37b5eed78b8ff39ab5f901902e275
Igor Zanella <igorzanella.dev@gmail.com>
2021-08-30 21:32:44 +0200

    style: better code style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import GlobalWrapper from "components/core/GlobalWrapper";
 import Last5PostsSlider from "components/ui/extended/Last5PostsSlider";
 import HomeFeaturedPost from "components/ui/extended/HomeFeaturedPost";
