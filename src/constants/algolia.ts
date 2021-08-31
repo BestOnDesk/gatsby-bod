340e05707133bf85405880b8714b4bbcfc6a5cb0
Igor Zanella <igorzanella.dev@gmail.com>
2021-08-31 18:55:46 +0200

    feat: remove ALGOLIA_ADMIN_KEY from constants

--- src/constants/algolia.ts
+++ src/constants/algolia.ts
@@ -2,5 +2,4 @@ export const ALGOLIA_INDEX_NAME =
   process.env.GATSBY_ALGOLIA_INDEX_NAME || "Posts";
 export const ALGOLIA_APP_ID = process.env.GATSBY_ALGOLIA_APP_ID || "";
 export const ALGOLIA_SEARCH_KEY = process.env.GATSBY_ALGOLIA_SEARCH_KEY || "";
-export const ALGOLIA_ADMIN_KEY = process.env.ALGOLIA_ADMIN_KEY || "";
 export const SEARCH_DISTINCT_LIMIT = 4;
